8e7b6c51dbe1dee8c5197d3a346f8deb325545d9
Nick van der Boor <nickyvanderboor@gmail.com>
2017-10-25 08:35:37 +0200

    test2

--- test1/GameViewController.swift
+++ test1/GameViewController.swift
@@ -13,7 +13,7 @@ class GameViewController: UIViewController {
 
     
     override func viewDidLoad() {
-        //remove most
+        //remove most info
         super.viewDidLoad()
         let scene = LevelOne(size: view.bounds.size)
         let skView = view as! SKView
